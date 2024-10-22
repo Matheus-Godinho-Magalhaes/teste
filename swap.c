0ed77900d19e97296642fc376c81ecffdcda9378
matheusgm <matheusgodinhomagalhaes@gmail.com>
2024-10-22 14:25:02 -0300

    Add a swap function

--- swap.c
+++ swap.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main(void)
 {
     int x = 3;
@@ -9,9 +15,7 @@ int main(void)
     printf("y is equals to %i\n", y);
 
     //swapping values using a temp variavel
-    int tmp = x;
-    x = y;
-    y = tmp;
+    swap(&x, &y);
 
     printf("x is now equals to %i\n", x);
     printf("y is now equals to %i\n", y);
