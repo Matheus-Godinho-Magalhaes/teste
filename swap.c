90a6d3657d4eb27e6313194ec0e34161b197ebb1
matheusgm <matheusgm@Matheus-Laptop.>
2024-10-22 12:35:47 -0300

    Creating swapping values feature - DONE

--- swap.c
+++ swap.c
@@ -9,9 +9,7 @@ int main(void)
     printf("y is equals to %i\n", y);
 
     //swapping values using a temp variavel
-    int tmp = x;
-    x = y;
-    y = tmp;
+    
 
     printf("x is now equals to %i\n", x);
     printf("y is now equals to %i\n", y);
