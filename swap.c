d90df2b5382ae6f021ba627f1756e4c917eadc58
matheusgm <matheusgodinhomagalhaes@gmail.com>
2024-10-22 12:49:05 -0300

    testando3

--- swap.c
+++ swap.c
@@ -9,7 +9,13 @@ int main(void)
     printf("y is equals to %i\n", y);
 
     //swapping values using a temp variavel
-    
+    int tmp = x;
+    x = y;
+    y = tmp;
+    int z = 4;
+    printf("testeando 1234\n");
+    printf("testeando 1234\n");
+    printf("testeando %i\n", z);
 
     printf("x is now equals to %i\n", x);
     printf("y is now equals to %i\n", y);
